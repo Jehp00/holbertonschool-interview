264871ed3758224ef939fdf907754388c909ae1d
Jehp00 <jehp01104006@gmail.com>
2023-03-31 16:17:09 -0500

    sorted list algorithm

---
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *new;
+    listint_t *current;
+
+    if (head == NULL)
+        return (NULL);
+
+    current = *head;
+
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->n = number;
+
+    if (*head == NULL)
+        *head = new;
+    else if ((*(head))->n >= number)
+    {
+        new->next = *head;
+        *head = new;
+    }
+    else
+    {
+        while (current->next != NULL && current->next->n < number)
+            current = current->next;
+        new->next = current->next;
+        current->next = new;
+    }
+    return (new);
+}
+
