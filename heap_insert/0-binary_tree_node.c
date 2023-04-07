69d88ca0cd973d918764afd8b6dff1dcad151f9b
Jehp00 <jehp01104006@gmail.com>
2023-04-06 20:09:39 -0500

    binary tree

---
+++ heap_insert/0-binary_tree_node.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_node - create a binary tree node
+ *
+ * @value: The value of the new node
+ * @parent: Pointer to the parent node
+ * Return: A pointer to the new node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new;
+
+    new = calloc(1, sizeof(binary_tree_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->n = value;
+    new->parent = parent;
+
+    return (new);
+}
+
