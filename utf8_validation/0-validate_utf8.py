f0d0ab5111e388107ff7f90a175835a6863bfa1c
Jehp00 <jehp01104006@gmail.com>
2023-05-26 16:56:42 -0500

    ENCODING UTF-8

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -7,43 +7,32 @@ def validUTF8(data):
     :param data: number or list of number to check
     :return: True if is utf-8, otherwise false
     """
-    NUMBER_OF_BITS_PER_BLOCK = 8
-    MAX_NUMBER_OF_ONES = 4
-    index = 0
-    while index < len(data):
-        number = data[index] & (2 ** 7)
-        number >>= (NUMBER_OF_BITS_PER_BLOCK - 1)
-        if number == 0:
-            index += 1
-            continue
-        number_of_ones = 0
-        while True:
-            number = data[index] & (2 ** (7 - number_of_ones))
-            number >>= (NUMBER_OF_BITS_PER_BLOCK - number_of_ones - 1)
-            if number == 1:
-                number_of_ones += 1
-            else:
-                break
-            if number_of_ones > MAX_NUMBER_OF_ONES:
-                return False
-        if number_of_ones == 1:
-            return False
-        index += 1
-        if index >= len(data) or index >= (index + number_of_ones - 1):
-            return False
+    num_bytes = 0
 
-        for i in range(index, index + number_of_ones - 1):
-            if data[i] > index:
+    for byte in data:
+        # Check if the most significant bit (MSB) is set
+        if num_bytes == 0:
+            # Count the number of leading ones in the byte
+            mask = 1 << 7
+            while mask & byte:
+                num_bytes += 1
+                mask >>= 1
+
+            # A single byte character
+            if num_bytes == 0:
+                continue
+
+            # Invalid UTF-8 character
+            if num_bytes == 1 or num_bytes > 4:
                 return False
-            number = data[i]
-            number >>= (NUMBER_OF_BITS_PER_BLOCK - 1)
-            if number != 1:
+        else:
+            # Check if the byte is a valid continuation byte
+            if byte >> 6 != 0b10:
                 return False
-            number >>= (NUMBER_OF_BITS_PER_BLOCK - 1)
-            if number != 0:
-                return False
-            index += 1
-    return True
+
+        # Decrement the number of remaining bytes to process
+        num_bytes -= 1
+    return num_bytes == 0
 
 """if __name__ == '__main__':
     validUTF8 = __import__('0-validate_utf8').validUTF8
