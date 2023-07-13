576f336d7ae1583992cb70293e5250c1e75ed6f9
Jehp00 <jehp01104006@gmail.com>
2023-07-13 17:54:03 -0500

    API star wars

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -2,22 +2,22 @@
 
 const request = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
-request(url, async function(error, res, body) {
-    if (error){
-        return console.log(error);
-    } else {
-        const characters = JSON.parse(body).characters;
-        for (const character in characters) {
-            const resp = await new Promise((resolve, reject) => {
-                request(characters[character], (err, res, html) =>{
-                    if (err){
-                        reject(err);
-                    } else {
-                        resolve(JSON.parse(html).name);
-                    }
-                });
-            });
-            console.log(res);
-        }
+request(url, async function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character in characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(characters[character], (err, res, html) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
+      });
+      console.log(res);
     }
+  }
 });
